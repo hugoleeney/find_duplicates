07902674519260b01a8af284bab44a6af1552495
Hugo Leeney <62838185+hugoleeney@users.noreply.github.com>
2021-03-29 11:32:00 +0100

    build_file_hashes first commit

---
+++ build_file_hashes.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import argparse
+import pathlib
+import filecmp
+import hashlib
+from uwalk import uwalk
+
+
+description = """
+Traverse provided directory and build a directory of hash codes to files.
+"""
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('--d', required=True, help="a directory to look in")
+parser.add_argument('-n', '--dont_explore_names', action="append", default=[], help="directory names not to descend into")
+parser.add_argument('-p', '--dont_explore_paths', action="append", default=[], help="directory paths not to descend into")
+parser.add_argument('-o', '--output_file', default='.hashdir.json', help='name of the file to output')
+parser.add_argument('-f', '--force', action='store_true', help='dont ask to overwrite existing output file')
+args = parser.parse_args()
+
+
+def get_file_md5(file_path):
+    with open(file_path, 'rb') as opened_file:
+        readFile = opened_file.read()
+        md5Hash = hashlib.md5(readFile)
+        md5Hashed = md5Hash.hexdigest()
+        return md5Hashed
+
+
+d = {}
+for dirpath, dirnames, files in os.walk(args.d):
+    for name in files:
+        file_path = os.path.join(dirpath, name)
+        d.setdefault(get_file_md5(file_path), []).append(file_path)
+
+
+if os.path.isfile(args.output_file) and not args.force:
+    userinput = 'a'
+    while userinput.lower() not in ['y', 'n', '']:
+        userinput = input('The output file specified already exists. Do you want to overwrite? [Y/n]')
+    if userinput == 'n':
+        print('exiting..')
+        exit()
+
+with open(args.output_file, 'w') as of:
+    json.dump(d, of)
