e239a7115ca4ee38df62a1fcc8ecafa3da90a3a4
hugoleeney <hugoleeney@users.noreply.github.com>
2021-03-30 11:27:14 +0100

    add main section to uwalk

--- uwalk.py
+++ uwalk.py
@@ -104,3 +104,15 @@ def uwalk(top, topdown=True, onerror=None, followlinks=False, start_at=None,
 
         if not start_at:
             yield top, dirs, nondirs
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('-b', '--bottomup', action="store_true", help="walk bottom up")
+    parser.add_argument('-s', '--start_at', help='path where to start')
+    parser.add_argument('-p', '--dont_explore_paths', action="append", default=[], help="directory paths not to descend into")
+    args = parser.parse_args()
+
+
+    for f in uwalk('./', not args.bottomup, start_at=args.start_at, dont_explore_paths=args.dont_explore_paths):
+        print(f)
